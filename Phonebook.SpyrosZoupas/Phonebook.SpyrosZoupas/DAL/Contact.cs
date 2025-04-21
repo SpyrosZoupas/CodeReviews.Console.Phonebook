b977bff0e69349021709306281c70a3ef52d5948
Spiros <spiroszoupas@hotmail.com>
2025-04-21 13:44:19 +0100

    OnConfiguring method on ContactContext, create Contact model

---
+++ Phonebook.SpyrosZoupas/Phonebook.SpyrosZoupas/DAL/Contact.cs
@@ -0,0 +1,10 @@
+﻿namespace Phonebook.SpyrosZoupas.DAL
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
--- Phonebook.SpyrosZoupas/Phonebook.SpyrosZoupas/DAL/ContactContext.cs
+++ Phonebook.SpyrosZoupas/Phonebook.SpyrosZoupas/DAL/ContactContext.cs
@@ -4,6 +4,9 @@ namespace Phonebook.SpyrosZoupas.DAL
 {
     public class ContactContext : DbContext // represents a session with the DB allows you to query the DB
     {
+        public DbSet<Contact> Contacts { get; set; } // represents a Contact table. Contact model is a row of Contact table
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSqlServer($"Data Source=(LocalDb)\\TheCSharpAcademy");
     }
 }
