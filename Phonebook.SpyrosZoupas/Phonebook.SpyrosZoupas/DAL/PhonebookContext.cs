5532f43587b4e8a412454a0962fc23f3eb2299d3
Spiros <spiroszoupas@hotmail.com>
2025-04-23 00:01:52 +0100

    - Initial OnModelCreating method

--- Phonebook.SpyrosZoupas/Phonebook.SpyrosZoupas/DAL/PhonebookContext.cs
+++ Phonebook.SpyrosZoupas/Phonebook.SpyrosZoupas/DAL/PhonebookContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Phonebook.SpyrosZoupas.DAL.Models;
+using System;
 
 namespace Phonebook.SpyrosZoupas.DAL
 {
@@ -12,5 +13,82 @@ namespace Phonebook.SpyrosZoupas.DAL
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
             optionsBuilder.UseSqlServer($"Server=(LocalDb)\\TheCSharpAcademy;Database=Phonebook;Trusted_Connection=True;");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Contact>()
+                .HasOne(p => p.Category)
+                .WithMany(c => c.Contacts)
+                .HasForeignKey(p => p.CategoryId);
+
+            modelBuilder.Entity<Category>()
+                .HasData(new List<Category>
+                {
+                new() {
+                    CategoryId = 1,
+                    Name = "Family"
+                },
+                new() {
+                    CategoryId = 2,
+                    Name = "Friends"
+                },
+                new() {
+                    CategoryId = 3,
+                    Name = "Coworkers"
+                },
+                new() {
+                    CategoryId = 4,
+                    Name = "Strangers"
+                }
+                });
+
+            modelBuilder.Entity<Contact>()
+                .HasData(new List<Contact>
+                {
+                    new() {
+                        ContactId = 1,
+                        CategoryId = 1,
+                        Name = "Dummy Name 1",
+                        PhoneNumber = "+4412345678901",
+                        Email = "dummy@email.com"
+                    },
+                    new() {
+                        ContactId = 2,
+                        CategoryId = 1,
+                        Name = "Dummy Name 2",
+                        PhoneNumber = "+4412345678901",
+                        Email = "dummy@email.com"
+                    },
+                    new() {
+                        ContactId = 3,
+                        CategoryId = 2,
+                        Name = "Dummy Name 3",
+                        PhoneNumber = "+4412345678901",
+                        Email = "dummy@email.com"
+                    },
+                    new() {
+                        ContactId = 4,
+                        CategoryId = 3,
+                        Name = "Dummy Name 4",
+                        PhoneNumber = "+4412345678901",
+                        Email= "dummy@email.com"
+                    },
+                    new() {
+                        ContactId = 5,
+                        CategoryId = 4,
+                        Name = "Dummy Name 5",
+                        PhoneNumber = "+4412345678901",
+                        Email="dummy@email.com"
+                    },
+                    new() {
+                        ContactId = 6,
+                        CategoryId = 1,
+                        Name = "Dummy Name 6",
+                        PhoneNumber = "+4412345678901",
+                        Email="dummy@email.com"
+                    }
+                });
+
+        }
     }
 }
